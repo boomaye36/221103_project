30bfe1bd9e698bfe690a553dc0d0720fc6b044bd
송현근 <>
2022-11-09 16:46:11 +0900

    boomaye36/221103_project#2. admin_gym db insert 도중 400 에러

--- src/test/java/com/justlift/ApplicationTests.java
+++ src/test/java/com/justlift/ApplicationTests.java
@@ -1,13 +1,27 @@
 package com.justlift;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApplicationTests {
 
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void 더하기테스트() {
+		log.debug("####### 더하기 테스트 수행");
+		int a = 10;
+		int b = 20;
+		assertEquals(30, a + b);
 	}
 
 }
